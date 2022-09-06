5e5632143c311f7408a1217911d6074f4f74b4f5
kaungkhantjc <kaungkhantjc@gmail.com>
2022-09-06 16:56:50 +0630

    added custom installer

--- install.sh
+++ install.sh
@@ -12,131 +12,152 @@ cur_dir=$(pwd)
 
 # check os
 if [[ -f /etc/redhat-release ]]; then
-    release="centos"
+  release="centos"
 elif cat /etc/issue | grep -Eqi "debian"; then
-    release="debian"
+  release="debian"
 elif cat /etc/issue | grep -Eqi "ubuntu"; then
-    release="ubuntu"
+  release="ubuntu"
 elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
-    release="centos"
+  release="centos"
 elif cat /proc/version | grep -Eqi "debian"; then
-    release="debian"
+  release="debian"
 elif cat /proc/version | grep -Eqi "ubuntu"; then
-    release="ubuntu"
+  release="ubuntu"
 elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
-    release="centos"
+  release="centos"
 else
-    echo -e "${red}System version not detected, please contact the script author! ${plain}\n" && exit 1
+  echo -e "${red}System version not detected, please contact the script author! ${plain}\n" && exit 1
 fi
 
 arch=$(arch)
 
 if [[ $arch == "x86_64" || $arch == "x64" || $arch == "amd64" ]]; then
-    arch="amd64"
+  arch="amd64"
 elif [[ $arch == "aarch64" || $arch == "arm64" ]]; then
-    arch="arm64"
+  arch="arm64"
 elif [[ $arch == "s390x" ]]; then
-    arch="s390x"
+  arch="s390x"
 else
-    arch="amd64"
-    echo -e "${red}Failed to check architecture, use default architecture: ${arch}${plain}"
+  arch="amd64"
+  echo -e "${red}Failed to check architecture, use default architecture: ${arch}${plain}"
 fi
 
 echo "Architecture: ${arch}"
 
 if [ "$(getconf WORD_BIT)" != '32' ] && [ "$(getconf LONG_BIT)" != '64' ]; then
-    echo "This software does not support 32-bit system (x86), please use 64-bit system (x86_64), if there are errors in the detection, please contact the author."
-    exit 0
+  echo "This software does not support 32-bit system (x86), please use 64-bit system (x86_64), if there are errors in the detection, please contact the author."
+  exit 1
 fi
 
 os_version=""
 
 # os version
 if [[ -f /etc/os-release ]]; then
-    os_version=$(awk -F'[= ."]' '/VERSION_ID/{print $3}' /etc/os-release)
+  os_version=$(awk -F'[= ."]' '/VERSION_ID/{print $3}' /etc/os-release)
 fi
 if [[ -z "$os_version" && -f /etc/lsb-release ]]; then
-    os_version=$(awk -F'[= ."]+' '/DISTRIB_RELEASE/{print $2}' /etc/lsb-release)
+  os_version=$(awk -F'[= ."]+' '/DISTRIB_RELEASE/{print $2}' /etc/lsb-release)
 fi
 
 if [[ x"${release}" == x"centos" ]]; then
-    if [[ ${os_version} -le 6 ]]; then
-        echo -e "${red}Please use CentOS 7 or higher version of the system! ${plain}\n" && exit 1
-    fi
+  if [[ ${os_version} -le 6 ]]; then
+    echo -e "${red}Please use CentOS 7 or higher version of the system! ${plain}\n" && exit 1
+  fi
 elif [[ x"${release}" == x"ubuntu" ]]; then
-    if [[ ${os_version} -lt 16 ]]; then
-        echo -e "${red}Please use Ubuntu 16 or higher version system! ${plain}\n" && exit 1
-    fi
+  if [[ ${os_version} -lt 16 ]]; then
+    echo -e "${red}Please use Ubuntu 16 or higher version system! ${plain}\n" && exit 1
+  fi
 elif [[ x"${release}" == x"debian" ]]; then
-    if [[ ${os_version} -lt 8 ]]; then
-        echo -e "${red}Please use Debian 8 or later system! ${plain}\n" && exit 1
-    fi
+  if [[ ${os_version} -lt 8 ]]; then
+    echo -e "${red}Please use Debian 8 or later system! ${plain}\n" && exit 1
+  fi
 fi
 
 install_base() {
-    if [[ x"${release}" == x"centos" ]]; then
-        yum install wget curl tar -y
-    else
-        apt install wget curl tar -y
-    fi
+  echo -e "\n${green}Installing wget, curl, tar ...${plain}\n"
+  if [[ x"${release}" == x"centos" ]]; then
+    yum install wget curl tar -y
+  else
+    apt install wget curl tar -y
+  fi
 }
 
 #This function will be called when user installed x-ui out of sercurity
 config_after_install() {
-    echo -e "${yellow}For security reasons, please change the port and login details. ${plain}"
-    echo -e "Port : ${green}54321${plain}"
-    echo -e "Username : ${green}admin${plain}"
-    echo -e "Password : ${green}admin${plain}"
+  echo -e "${yellow}For security reasons, please change the port and login details. ${plain}"
+  echo -e "\nPort : ${green}54321${plain}"
+  echo -e "Username : ${green}admin${plain}"
+  echo -e "Password : ${green}admin${plain}"
 }
 
 install_x-ui() {
-    systemctl stop x-ui
-    cd /usr/local/ || return
+  echo -e "\n${green}Installing Xray Panel ...${plain}\n"
 
-    if [ $# == 0 ]; then
-        last_version=$(curl -Ls "https://api.github.com/repos/vaxilu/x-ui/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-        if [[ -z "$last_version" ]]; then
-            echo -e "${red}Failed to detect x-ui version, it may exceed the Github API limit, please try again later, or install x-ui version manually. ${plain}"
-            exit 1
-        fi
-        echo -e "Detected x-ui latest version: ${last_version}，starting installation.."
-        wget -N --no-check-certificate -O /usr/local/x-ui-linux-${arch}.tar.gz https://github.com/vaxilu/x-ui/releases/download/${last_version}/x-ui-linux-${arch}.tar.gz
-        if [[ $? -ne 0 ]]; then
-            echo -e "${red}Download x-ui failed, please make sure your server can download the Github file. ${plain}"
-            exit 1
-        fi
-    else
-        last_version=$1
-        url="https://github.com/vaxilu/x-ui/releases/download/${last_version}/x-ui-linux-${arch}.tar.gz"
-        echo -e "Start installation x-ui v$1"
-        wget -N --no-check-certificate -O /usr/local/x-ui-linux-${arch}.tar.gz ${url}
-        if [[ $? -ne 0 ]]; then
-            echo -e "${red}Download x-ui v$1 failed, please make sure this version exists. ${plain}"
-            exit 1
-        fi
+  systemctl stop x-ui
+  cd /usr/local/ || return
+
+  if [ $# == 0 ]; then
+    last_version=$(curl -Ls "https://api.github.com/repos/vaxilu/x-ui/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+    if [[ ! -n "$last_version" ]]; then
+      echo -e "${red}Failed to detect x-ui version, it may exceed the Github API limit, please try again later, or install x-ui version manually. ${plain}"
+      exit 1
     fi
-
-    if [[ -e /usr/local/x-ui/ ]]; then
-        rm /usr/local/x-ui/ -rf
+    echo -e "Detected x-ui latest version: ${last_version}，starting installation.."
+    wget -N --no-check-certificate -O /usr/local/x-ui-linux-${arch}.tar.gz https://github.com/vaxilu/x-ui/releases/download/${last_version}/x-ui-linux-${arch}.tar.gz
+    if [[ $? -ne 0 ]]; then
+      echo -e "${red}Download x-ui failed, please make sure your server can download the Github file. ${plain}"
+      exit 1
     fi
+  else
+    last_version=$1
+    url="https://github.com/vaxilu/x-ui/releases/download/${last_version}/x-ui-linux-${arch}.tar.gz"
+    echo -e "Start installation x-ui v$1"
+    wget -N --no-check-certificate -O /usr/local/x-ui-linux-${arch}.tar.gz ${url}
+    if [[ $? -ne 0 ]]; then
+      echo -e "${red}Download x-ui v$1 failed, please make sure this version exists. ${plain}"
+      exit 1
+    fi
+  fi
 
-    tar zxvf x-ui-linux-${arch}.tar.gz
-    rm x-ui-linux-${arch}.tar.gz -f
-    cd x-ui || return
-    chmod +x x-ui bin/xray-linux-${arch}
-    cp -f x-ui.service /etc/systemd/system/
-    wget --no-check-certificate -O /usr/bin/x-ui https://raw.githubusercontent.com/vaxilu/x-ui/main/x-ui.sh
-    chmod +x /usr/local/x-ui/x-ui.sh
-    chmod +x /usr/bin/x-ui
-    config_after_install
+  if [[ -e /usr/local/x-ui/ ]]; then
+    rm /usr/local/x-ui/ -rf
+  fi
 
-    systemctl daemon-reload
-    systemctl enable x-ui
-    systemctl start x-ui
-    echo -e "${green}x-ui v${last_version}${plain} The installation is completed and the panel is activated."
-    printf "Proudly simplified the script by %bKaung Khant Kyaw \uD83D\uDE0A" "$green"
+  tar zxvf x-ui-linux-${arch}.tar.gz
+  rm x-ui-linux-${arch}.tar.gz -f
+  cd x-ui || return
+  chmod +x x-ui bin/xray-linux-${arch}
+  cp -f x-ui.service /etc/systemd/system/
+  wget --no-check-certificate -O /usr/bin/x-ui https://raw.githubusercontent.com/vaxilu/x-ui/main/x-ui.sh
+  chmod +x /usr/local/x-ui/x-ui.sh
+  chmod +x /usr/bin/x-ui
+  config_after_install
+
+  systemctl daemon-reload
+  systemctl enable x-ui
+  systemctl start x-ui
+  echo -e "${green}x-ui v${last_version}${plain} The installation is completed and the panel is activated."
+  echo ""
+  echo "------------------------------------"
+  printf "  Proudly simplified the script by  \n"
+  echo "------------------------------------"
+  echo ""
+
+  echo -e "\033[0;32m
+ _  __                         _  ___                 _     _  __
+| |/ /                        | |/ / |               | |   | |/ /
+| ' / __ _ _   _ _ __   __ _  | ' /| |__   __ _ _ __ | |_  | ' /_   _  __ ___      __
+|  < / _\` | | | | '_ \ / _\` | |  < | '_ \ / _\` | '_ \| __| |  <| | | |/ _\` \ \ /\ / /
+| . \ (_| | |_| | | | | (_| | | . \| | | | (_| | | | | |_  | . \ |_| | (_| |\ V  V /
+|_|\_\__,_|\__,_|_| |_|\__, | |_|\_\_| |_|\__,_|_| |_|\__| |_|\_\__, |\__,_| \_/\_/
+                        __/ |                                    __/ |
+                       |___/                                    |___/
+${plain}"
+  echo -e "(ɔ◔‿◔)ɔ \033[0;31m ♥${plain}"
+  echo ""
+  echo ""
+
 }
 
-echo -e "${green}Installing Xray Panel ...${plain}"
 install_base
-install_x-ui "$1"
+install_x-ui $1
